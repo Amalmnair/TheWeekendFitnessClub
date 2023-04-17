bbecc84cbdf24d5bd0f6cbbfce097d53f24a4669
amal <amalmnair07@gmail.com>
2023-04-17 23:48:19 +0100

    Booking Class for booking operation

---
+++ src/Booking.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Booking {
+
+    static int classCode;
+    static void BookView() {
+
+        Scanner sc=new Scanner(System.in);
+        int classChoice,dayChoice,fitChoice;
+        System.out.println("\nBook a Group Fitness Lesson");
+        System.out.println("------------------------------\n");
+        System.out.println("Choose your Class by");
+        System.out.println("1. Day\n2. Fitness Type\n");
+        classChoice = sc.nextInt();
+        if (classChoice == 1) {
+            System.out.println("\nChoose Day");
+            System.out.println("1. Saturday\n2. Sunday\n");
+            dayChoice = sc.nextInt();
+            classCode=TimeTable.TimetableDay(dayChoice);
+            CheckSeats();
+        } else if (classChoice == 2) {
+            System.out.println("\nChoose Class");
+            System.out.println("1. Yoga\n2. Box Fit\n3. Zumba\n4. Aquacise\n");
+            fitChoice = sc.nextInt();
+            classCode=TimeTable.TimetableClass(fitChoice);
+            CheckSeats();
+        } else
+            System.out.println("Invalid Choice :(");
+    }
+
+    static void CheckSeats(){
+
+        int seatLeft;
+        seatLeft=Database.SeatAvailable(classCode);
+        if (seatLeft<=0)
+            System.out.println("\nSorry No Seats Available :(\n");
+
+        else
+            BookSeat();
+
+    }
+
+
+    static void BookSeat(){
+
+        String name,id;
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("\nEnter your details\n");
+        System.out.print("Name:\t");
+        name=sc.nextLine();
+        System.out.print("ID:\t");
+        id=sc.nextLine();
+        Database.SelectBookingClass(name,id,classCode);
+
+
+
+    }
+
+
+
+}
