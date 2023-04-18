f0a81b151b738b18e0a36184191e64637027213f
amal <amalmnair07@gmail.com>
2023-04-18 13:14:24 +0100

    Testing Intial stage

---
+++ src/DatabaseTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseTest {
+
+    @Test
+    public void seatAvailable() {
+        Database d= new Database();
+        assertEquals(0,d.SeatAvailable(15));
+        assertEquals(d.s11,d.SeatAvailable(11));
+
+    }
+}
