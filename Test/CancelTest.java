acd3706095cdd94ca41cc457f880a37c94de16d7
amal <amalmnair07@gmail.com>
2023-04-21 20:05:43 +0100

    cancel class testing

--- Test/CancelTest.java
+++ Test/CancelTest.java
@@ -2,13 +2,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 class CancelTest {
-
     @Test
-    void cancelView() {
+    void test1cancelView() {
 
         ByteArrayInputStream userInput1 = new ByteArrayInputStream("111\n1\n2".getBytes());
         System.setIn(userInput1);
@@ -16,6 +13,17 @@ class CancelTest {
         System.setIn(System.in);
 
             }
+
+    @Test
+    void test2cancelView() {
+
+        ByteArrayInputStream userInput1 = new ByteArrayInputStream("0000".getBytes());
+        System.setIn(userInput1);
+        Cancel.CancelView();
+        System.setIn(System.in);
+
+    }
+
 
 
 }
